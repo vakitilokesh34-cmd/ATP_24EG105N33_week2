e0ff58f612215a50b23771d4866afa1482297b5f
VAKITI LOKESH <vakitilokesh34@gmail.com>
2026-05-15 13:39:37 +0530

    Delete TaskManagementSystem directory

--- TaskManagementSystem/app.js
+++
@@ -1,7 +0,0 @@
-import {addTask,getAllTasks} from './task.js'
-
-addTask("Eating","high",'2024-12-12')
-let task2=addTask("Sleeping","high",'2027-12-12')
-console.log(task2);
-let rs=getAllTasks()
-console.log(rs);
